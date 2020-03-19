08ff88ac008e01a195db0d1e12959c5805474c65
Al Simons <al@simonshome.org>
2020-03-19 04:59:45 +0100

    Add options for country and state.
    Also fix column header alignment bug, and handle case of empty fields in input.

--- us_tracker.py
+++ us_tracker.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import os
 import sys
@@ -5,10 +6,19 @@ import sys
 
 BASE_DIR = '.'
 FIXED_DIRS = r'COVID-19\csse_covid_19_data\csse_covid_19_time_series'
-COUNTRY = 'US'
 
 
-def process_file(country, file_path):
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--country', default="US",
+                        help="The name of the country to summarize")
+    parser.add_argument('-s', '--state',
+                        help="State to monitor if country is US")
+    args = parser.parse_args()
+    return args
+
+
+def process_file(country, state, file_path):
     with open(os.path.join(BASE_DIR, FIXED_DIRS, file_path)) as in_csv:
         reader = csv.reader(in_csv)
 
@@ -16,8 +26,12 @@ def process_file(country, file_path):
         accumulator = [0] * len(dates)
         for line in reader:
             if line[1] == country:
+                if state and country == 'US' and line[0] != state:
+                    continue
                 counts = line[4:]
                 for n in range(len(dates)):
+                    if counts[n] == '':
+                        continue
                     accumulator[n] += int(counts[n])
 
     new_cases = [0] * len(dates)
@@ -27,30 +41,42 @@ def process_file(country, file_path):
 
 
 def main():
+    args = parse_args()
+
     # Confirmed cases
+    country = args.country
+    state = args.state
+
     confirmed_dates, confirmed_new_cases, confirmed_cases = \
-        process_file(COUNTRY, 'time_series_19-covid-Confirmed.csv')
+        process_file(country, state, 'time_series_19-covid-Confirmed.csv')
+
     deaths_dates, new_deaths, cumulative_deaths = \
-        process_file(COUNTRY, 'time_series_19-covid-Deaths.csv')
+        process_file(country, state, 'time_series_19-covid-Deaths.csv')
+
     recovered_dates, new_recovered, cumulative_recovered = \
-        process_file(COUNTRY, 'time_series_19-covid-Recovered.csv')
+        process_file(country, state, 'time_series_19-covid-Recovered.csv')
+
     if (confirmed_dates[-1] != deaths_dates[-1] or
             confirmed_dates[-1] != recovered_dates[-1]):
         sys.exit("Error: dates don't match. Exiting.\n{}\n{}\n{}".format(
                  confirmed_dates[-1], deaths_dates[-1], recovered_dates[-1]))
 
+    title = country if not state else state
+
     with open("output.txt", 'w') as f:
-        print("Confirmed\t\tDeaths\t\tRecovered", file=f)
+        print("{}\tConfirmed\t\tDeaths\t\tRecovered".format(title), file=f)
         print("Date\tCumulative\tNew\tCumulative\tNew\tCumulative\tNew",
               file=f)
         for n in range(len(confirmed_dates)):
-            print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(confirmed_dates[n],
-                                      confirmed_cases[n],
-                                      confirmed_new_cases[n],
-                                      cumulative_deaths[n],
-                                      new_deaths[n],
-                                      cumulative_recovered[n],
-                                      new_recovered[n]), file=f)
+            print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+                confirmed_dates[n],
+                confirmed_cases[n],
+                confirmed_new_cases[n],
+                cumulative_deaths[n],
+                new_deaths[n],
+                cumulative_recovered[n],
+                new_recovered[n]), file=f)
+
 
 if __name__ == '__main__':
     main()
